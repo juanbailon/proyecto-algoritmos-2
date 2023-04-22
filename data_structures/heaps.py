2331debb99955c15600cd53efe1725d0093edfa0
juanBailon <juanojo2001.5@gmail.com>
2023-04-22 16:10:58 -0500

    docs MinHeap class

--- data_structures/heaps.py
+++ data_structures/heaps.py
@@ -2,10 +2,21 @@ import math
 from typing import Union
 
 class MinHeap(object):
+    """ 
+    This is a minimum heap implementation, it was design whit the purpose
+    of being a heap of numbers, either int or float.
+
+    warning:
+        this is not a complete/full heap implentation, since it lacks of heap
+        operations such as remove 
+    """
 
     def __init__(self) -> None:
         self.size = 0
         self.items = []
+
+    def get_item(self, index: int) -> Union[int, float]:
+        return self.items[index]
 
     def get_left_child_index(self, parent_index: int) -> int:
         return 2*parent_index + 1
@@ -44,12 +55,14 @@ class MinHeap(object):
         self.items[indexB] = temp
 
     def peek(self) -> Union[int, float]:
+        """ Returns the values of the first element of the heap whitout removing it """
         if(self.size==0):
             raise Exception(f'No element to peek, heap size = {self.size}')
         else:
             return self.items[0]
             
     def poll(self) -> Union[int, float]:
+        """ Removes the first elments of the heap """
         if(self.size==0):
             raise Exception(f'No element to peek, heap size = {self.size}')
         
@@ -60,12 +73,14 @@ class MinHeap(object):
     
 
     def add(self, item: Union[int, float]) -> None:
+        """ Adds a new item to the heap """
         self.size += 1
         self.items.append(item)
         self.heapify_up()
 
 
     def heapify_down(self) -> None:
+        """ Takes the firts element of the heap  and heapify this elements down the heap """
         index = 0 #cero bacuse we start at the root
 
         """ we just check for left child becuse if there is not left
@@ -76,7 +91,7 @@ class MinHeap(object):
             if( self.has_right_child(index) and self.get_right_child(index) < self.get_left_child(index) ):
                 smaller_child_index = self.get_right_child_index(index)  
 
-            if(self.items[index] and self.items[smaller_child_index]):
+            if(self.items[index] < self.items[smaller_child_index]):
                 break
             else:
                 self.swap(index, smaller_child_index)
@@ -85,12 +100,17 @@ class MinHeap(object):
 
 
     def heapify_up(self) -> None:
+        """ Takes the last element of the heap  and heapify this elements up the heap """
         index = self.size - 1
-        
-        while(self.has_parent(index) and self.get_parent(index) > self.items[index]):
+
+        while(self.has_parent(index) and (self.get_parent(index) > self.items[index]) ):
             self.swap(self.get_parent_index(index), index)
             index = self.get_parent_index(index)
 
     
-    def __str__(self) -> str:
+    def __str__(self) -> str:        
         return f'MinHeap-items: {self.items}'
+    
+
+    
+    
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from data_structures.heaps import MinHeap
 
-
 def main():
     my_heap =MinHeap()
     
@@ -10,10 +9,7 @@ def main():
     my_heap.add(25)
     my_heap.add(15)
 
-
     print(my_heap)
-    print(my_heap.size)
-    print(my_heap.get_parent_index(1))
 
 
 if __name__ == '__main__':
